46fa526afd40492044d03bf007031a920e907a16
hugodrevet <hugo.drevet@hotmail.fr>
2025-05-12 17:22:22 +0200

    pushed new test

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def hello(firstname="John", lastname="Doe"):
     if not isinstance(firstname, str) or not isinstance(lastname, str):
         raise TypeError(
-            "Les paramètres 'firstname' et 'lastname' doivent être des chaînes de caractères."
+            "Les paramètres 'firstname' et 'lastname' doivent être des chaînes "
+            "de caractères."
         )
     return f"Hello, {firstname} {lastname}!"
